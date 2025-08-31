9e750822c59862c84ef6723bd71f2eb262efc7b1
ibrahim mohammed <afrixaai@gmail.com>
2025-08-31 13:22:28 +0100

    updates on mail issue

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ document.addEventListener('DOMContentLoaded', function() {
             formData.append('_template', 'table');
             formData.append('_captcha', 'false');
             
-            fetch('https://formsubmit.co/ajax/afrixaai@gmail.com', {
+            fetch('https://formsubmit.co/el/cisumu', {
                 method: 'POST',
                 body: formData
             })
